09caca84b59018c875a282d85833b510f4376754
deliaspanigo <david.elias949@gmail.com>
2026-02-07 13:20:29 +0100

    Release: Versión 0.6.4 - Build.

--- script004_github_version.R
+++ script004_github_version.R
@@ -1,11 +1,11 @@
 # 1. Preparar y Guardar los cambios (incluyendo el DESCRIPTION limpio)
 git add .
-git commit -m "Release: Versión 0.6.3 - extra."
+git commit -m "Release: Versión 0.6.4 - Build."
 
 # 2. Etiquetar la versión
 # Borramos el tag local por si acaso ya se creó con error antes
-git tag -d v0.6.3 2>/dev/null
-git tag -a v0.6.3 -m "Versión estable 0.6.3 del paquete"
+git tag -d v0.6.4 2>/dev/null
+git tag -a v0.6.4 -m "Versión estable 0.6.4 del paquete"
 
 # 3. Subir cambios y etiquetas a GitHub
 git push origin main --follow-tags
